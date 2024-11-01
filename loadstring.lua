6e960299c8001a6ddf678f2451d042e304282051
cream <dthebestusernamelol+hilol123@gmail.com>
2024-11-01 22:20:56 +0800

    whatever

--- loadstring.lua
+++ loadstring.lua
@@ -1,3 +1,1 @@
-shared.VapePrivate = true
-shared.LoadSlowmode = 0.03
-loadstring(game:HttpGet("https://raw.githubusercontent.com/jjrangzx/Fraudware/refs/heads/main/NewMainScript.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jjrangzx/Dev/refs/heads/main/NewMainScript.lua", true))()
